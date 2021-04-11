c3cc18d10d6d8c590286fd3c193f8f9db9d21999
FranSpinelli <franciscospinelli98@gmail.com>
2021-04-11 13:58:31 -0300

    [master] initial commit

---
+++ src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/ServletInitializer.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.desapp.grupoK.backenddesappapi;
+
+import ar.edu.unq.desapp.grupoK.backenddesappapi.BackendDesappApiApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendDesappApiApplication.class);
+    }
+
+}
