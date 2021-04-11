03a21ca509d3196c1e2ab061eddae2ab789fd404
FranSpinelli <franciscospinelli98@gmail.com>
2021-04-11 16:38:34 -0300

    [master] created a test in order to check travis integration and added travis .yml file

--- src/test/java/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoK/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -3,11 +3,15 @@ package ar.edu.unq.desapp.grupoK.backenddesappapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BackendDesappApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void basicTest() {
+        //Test creado para ver si triggerea Travis
+
+        assertEquals(1,1);
     }
 
 }
