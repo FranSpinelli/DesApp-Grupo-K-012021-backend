81f0b07aecb904d74fa290855893e2b16d1c2b4e
gradaglio <gradaglio@hotmail.com>
2021-04-25 21:05:02 -0300

    Eliminación de las clases Actor, Director, Writter

--- src/main/java/model/Director.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Director extends FilmWorker{
-
-    public Director(Integer anId, String aName, String aSurname, Integer aBirthYear, Integer aDeathYear)
-            throws InvalidDatesError{
-        super (anId, aName, aSurname, aBirthYear, aDeathYear);
-    }
-}
