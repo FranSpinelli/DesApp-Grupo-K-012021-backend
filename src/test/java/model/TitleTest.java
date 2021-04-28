fb3519ec446612ed2ea2121cfd5d2e3c8827c854
gradaglio <gradaglio@hotmail.com>
2021-04-28 00:00:01 -0300

    Refactor de los test por los cambios en el modelo

--- src/test/java/model/TitleTest.java
+++ src/test/java/model/TitleTest.java
@@ -9,7 +9,7 @@ class TitleTest {
     @Test
     void TitleConstructorAndGettersTestCase2() throws InvalidDatesError {
 
-        Title aMovie = new Movie(1,"Titanic", false, 1998, null,100);
+        Title aMovie = new Title(1,"Titanic", false, 1998, null,100, "movie");
 
         assertEquals(aMovie.getId(), 1);
         assertEquals(aMovie.getOriginalTitle(), "Titanic");
@@ -17,13 +17,22 @@ class TitleTest {
         assertEquals(aMovie.getStartYear(), 1998);
         assertNull(aMovie.getEndYear());
         assertEquals(aMovie.getRuntimeMinutes(),100);
+        assertEquals(aMovie.getType(), "movie");
     }
 
     @Test
     void TitleThrowsAnExceptionWhenInvalidDateIsReceivedCase1(){
-        //CASE 1: a movie is created with a start year is smaller than 0
+        //CASE 1: a title is created with a start year is smaller than 0
         assertThrows(InvalidDatesError.class, () -> {
-        new Movie(1,"Titanic", false, 0, null,100);
+        new Title(1,"Titanic", false, 0, null,100, "movie");
+        });
+    }
+
+    @Test
+    void TitleThrowsAnExceptionWhenInvalidDateIsReceivedCase2(){
+        //CASE 2: a title is created when an end year is bigger than current year
+        assertThrows(InvalidDatesError.class, () -> {
+            new Title(1,"Titanic", false, 0, 2022,100, "movie");
         });
     }
 }
