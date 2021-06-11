0f37622b0d85fd08f99735d855a40bfa47cf57fc
FranSpinelli <franciscospinelli98@gmail.com>
2021-06-11 19:41:42 -0300

    optimized imports

--- src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/GlobalExceptionHandler.java
+++ src/main/java/ar/edu/unq/desapp/grupoK/backenddesappapi/GlobalExceptionHandler.java
@@ -6,11 +6,9 @@ import ar.edu.unq.desapp.grupoK.backenddesappapi.service.serviceLevelExceptions.
 import ar.edu.unq.desapp.grupoK.backenddesappapi.service.serviceLevelExceptions.TokenValidationException;
 import ar.edu.unq.desapp.grupoK.backenddesappapi.webservice.WrongApiKeyException;
 import ar.edu.unq.desapp.grupoK.backenddesappapi.webservice.dto.EmptyDTOException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
